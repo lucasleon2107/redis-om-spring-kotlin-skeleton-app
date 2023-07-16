2b928a7cf642c31625cb6c0920e7c40490d75d8f
Lucas Leon <lleon@rccl.com>
2023-07-16 05:34:51 -0500

    Remove redundant explicit type

--- src/main/kotlin/com/redis/om/skeleton/OmKotlinSkeletonApplication.kt
+++ src/main/kotlin/com/redis/om/skeleton/OmKotlinSkeletonApplication.kt
@@ -37,44 +37,44 @@ class OmKotlinDemoApplication {
 			val nickFurySays = "Sir, I'm Gonna Have To Ask You To Exit The Donut"
 
 			// Serendipity, 248 Seven Mile Beach Rd, Broken Head NSW 2481, Australia
-			val thorsAddress: Address =
+			val thorsAddress =
 				Address("248", "Seven Mile Beach Rd", "Broken Head", "NSW", "2481", "Australia")
 
 			// 11 Commerce Dr, Riverhead, NY 11901
-			val ironmansAddress: Address = Address("11", "Commerce Dr", "Riverhead", "NY", "11901", "US")
+			val ironmansAddress = Address("11", "Commerce Dr", "Riverhead", "NY", "11901", "US")
 
 			// 605 W 48th St, New York, NY 10019
-			val blackWidowAddress: Address = Address("605", "48th St", "New York", "NY", "10019", "US")
+			val blackWidowAddress = Address("605", "48th St", "New York", "NY", "10019", "US")
 
 			// 20 W 34th St, New York, NY 10001
-			val wandaMaximoffsAddress: Address = Address("20", "W 34th St", "New York", "NY", "10001", "US")
+			val wandaMaximoffsAddress = Address("20", "W 34th St", "New York", "NY", "10001", "US")
 
 			// 107 S Beverly Glen Blvd, Los Angeles, CA 90024
-			val gamorasAddress: Address = Address("107", "S Beverly Glen Blvd", "Los Angeles", "CA", "90024", "US")
+			val gamorasAddress = Address("107", "S Beverly Glen Blvd", "Los Angeles", "CA", "90024", "US")
 
 			// 11461 Sunset Blvd, Los Angeles, CA 90049
-			val nickFuryAddress: Address = Address("11461", "Sunset Blvd", "Los Angeles", "CA", "90049", "US")
-			val thor: Person = Person(
+			val nickFuryAddress = Address("11461", "Sunset Blvd", "Los Angeles", "CA", "90049", "US")
+			val thor = Person(
 				null, "Chris", "Hemsworth", 38, thorSays, Point(153.616667, -28.716667), thorsAddress,
 				setOf("hammer", "biceps", "hair", "heart")
 			)
-			val ironman: Person = Person(
+			val ironman = Person(
 				null, "Robert", "Downey", 56, ironmanSays, Point(40.9190747, -72.5371874),
 				ironmansAddress, setOf("tech", "money", "one-liners", "intelligence", "resources")
 			)
-			val blackWidow: Person = Person(
+			val blackWidow = Person(
 				null, "Scarlett", "Johansson", 37, blackWidowSays, Point(40.7215259, -74.0129994),
 				blackWidowAddress, setOf("deception", "martial_arts")
 			)
-			val wandaMaximoff: Person = Person(
+			val wandaMaximoff = Person(
 				null, "Elizabeth", "Olsen", 32, wandaMaximoffSays, Point(40.6976701, -74.2598641),
 				wandaMaximoffsAddress, setOf("magic", "loyalty")
 			)
-			val gamora: Person = Person(
+			val gamora = Person(
 				null, "Zoe", "Saldana", 43, gamoraSays, Point(-118.399968, 34.073087), gamorasAddress,
 				setOf("skills", "martial_arts")
 			)
-			val nickFury: Person = Person(
+			val nickFury = Person(
 				null, "Samuel L.", "Jackson", 73, nickFurySays, Point(-118.4345534, 34.082615),
 				nickFuryAddress, setOf("planning", "deception", "resources")
 			)
